7b78ceec3995c417fe5236cf81ab591c2987ed27
Dongju Jang <dongju@cosmos.io>
2015-03-31 17:54:13 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var raven = require('raven');
+var request = require('request');
+var port = process.env.PORT || 8888;
+var url = process.env.WEBHOOK_URL || '';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(raven.middleware.express(process.env.SENTRY_DSN || ''));
+
+app.get('/*', function(req, res){
+
+  throw new Error('Broke!');
+  res.send(Error);
+});
+
+app.post('/post', function(req, res){
+  if (!req.body || !url) {
+    res.status(500).json('');
+    return;
+  }
+
+  res.json('');
+
+  JSON.stringify(req.body, null, 4);
+  var result = JSON.stringify(req.body, null, "\t");
+
+  var options = {
+    text: result,
+    username: 'build bot'
+  };
+  var requestParams = {
+    url: url,
+    body: JSON.stringify(options)
+  };
+  request.post(requestParams, function(err,res,body) {
+    console.log(req.body)
+  });
+});
+
+app.listen(port);
